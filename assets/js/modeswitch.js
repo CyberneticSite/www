7768087288b2cc6b36b8aca1b1d139e77295260d
Vulcan <93451215+trholding@users.noreply.github.com>
2024-06-06 19:54:59 +0530

    Update modeswitch.js

--- assets/js/modeswitch.js
+++ assets/js/modeswitch.js
@@ -2,19 +2,19 @@ let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
 
 if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-moon-o" aria-hidden="true"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-moon-stars"></i>";
 } else {
-	document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-sun-o" aria-hidden="true"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-sun"></i>";
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
-   	document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-moon-o" aria-hidden="true"></i>";
+   	document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-moon-stars"></i>";
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-sun-o" aria-hidden="true"></i>";
+    document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-sun"></i>";
     sessionStorage.setItem('theme', '');
   }
 }
@@ -26,28 +26,28 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-sun-o" aria-hidden="true"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-sun"></i>";
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-moon-o" aria-hidden="true"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-moon-stars"></i>";
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-sun-o" aria-hidden="true"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-sun"></i>";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-moon-o" aria-hidden="true"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-moon-stars"></i>";
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-moon-o" aria-hidden="true"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-moon-stars"></i>";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "<i class="fa fa-sun-o" aria-hidden="true"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fa-regular fa-sun"></i>";
 }
